dfcac57f4c69abe516f8767088635d7ef5052720
fafa2000 <32624303+fafa2000@users.noreply.github.com>
2018-01-15 14:08:01 -0500

    first version of program

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+package Summative;
+
+/**
+ *
+ * @author S199847600
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
